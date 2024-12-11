096a518c9ebdfeba5802e9e580cd9fdcdeae60fc
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2024-12-11 21:12:06 +0530

    Keyed State, Part 2

--- src/main/scala/part3state/KeyedState.scala
+++ src/main/scala/part3state/KeyedState.scala
@@ -1,7 +1,8 @@
 package part3state
 
 import generators.shopping._
-import org.apache.flink.api.common.state.{ListState, ListStateDescriptor, ValueState, ValueStateDescriptor}
+import org.apache.flink.api.common.state.{ListState, ListStateDescriptor, MapState, MapStateDescriptor, StateTtlConfig, ValueState, ValueStateDescriptor}
+import org.apache.flink.api.common.time.Time
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction
 import org.apache.flink.streaming.api.scala._
@@ -116,9 +117,95 @@ object KeyedState {
   }
 
   // MapState
+  def demoMapState(): Unit = {
+    // count how many events PER TYPE were ingested PER USER
+    val streamOfCountsPerType = eventsPerUser.process(
+      new KeyedProcessFunction[String, ShoppingCartEvent, String] {
+
+        // create the state
+        var stateCountsPerEventType: MapState[String, Long] = _ // keep this bounded
+
+        // initialize the state
+        override def open(parameters: Configuration): Unit = {
+          stateCountsPerEventType = getRuntimeContext.getMapState(
+            new MapStateDescriptor[String, Long](
+              "per-type-counter",
+              classOf[String],
+              classOf[Long]
+            )
+          )
+        }
+
+        override def processElement(
+                                     value: ShoppingCartEvent,
+                                     ctx: KeyedProcessFunction[String, ShoppingCartEvent, String]#Context,
+                                     out: Collector[String]): Unit = {
+          // fetch the type of the event
+          val eventType = value.getClass.getSimpleName
+          if (stateCountsPerEventType.contains(eventType)) {
+            val oldCount = stateCountsPerEventType.get(eventType)
+            val newCount = oldCount + 1
+            stateCountsPerEventType.put(eventType, newCount)
+          } else {
+            stateCountsPerEventType.put(eventType, 1)
+          }
+
+          // push some output
+          import scala.collection.JavaConverters._
+          out.collect(s"${ctx.getCurrentKey} - ${stateCountsPerEventType.entries().asScala.mkString(", ")}")
+        }
+      }
+    )
+
+    streamOfCountsPerType.print()
+    env.execute()
+  }
+
+  // clear the state manually
+  // clear the state at a regular interval
+  def demoListStateWithClearance(): Unit = {
+    // store all the events per user id
+    val allEventsPerUserStream = eventsPerUser.process(
+      new KeyedProcessFunction[String, ShoppingCartEvent, String] {
+
+        import scala.collection.JavaConverters._ // implicit converters (extension methods)
+
+        // if more than 10 elements, clear the first
+        var stateEventsForUser: ListState[ShoppingCartEvent] = _ // once per key
+        // you need to be careful to keep the size of the list BOUNDED
+
+        // initialization
+        override def open(parameters: Configuration): Unit = {
+          val descriptor = new ListStateDescriptor[ShoppingCartEvent]("shopping-cart-events", classOf[ShoppingCartEvent])
+          // time to live = cleared if it's not modified after a certain time
+          descriptor.enableTimeToLive(
+            StateTtlConfig.newBuilder(Time.hours(1)) // clears the state after 1hr
+              .setUpdateType(StateTtlConfig.UpdateType.OnCreateAndWrite) // specify when the timer resets
+              .setStateVisibility(StateTtlConfig.StateVisibility.ReturnExpiredIfNotCleanedUp)
+              .build()
+          )
+          stateEventsForUser = getRuntimeContext.getListState(descriptor)
+        }
+
+        override def processElement(
+                                     value: ShoppingCartEvent,
+                                     ctx: KeyedProcessFunction[String, ShoppingCartEvent, String]#Context,
+                                     out: Collector[String]): Unit = {
+          stateEventsForUser.add(value)
+          val currentEvents = stateEventsForUser.get().asScala.toList
+          if (currentEvents.size > 10) stateEventsForUser.clear() // clearing is not done immediately
+
+          out.collect(s"User ${value.userId} - [${currentEvents.mkString(", ")}]")
+        }
+      }
+    )
+
+    allEventsPerUserStream.print()
+    env.execute()
+  }
 
   def main(args: Array[String]): Unit = {
-    demoListState()
+    demoListStateWithClearance()
   }
 
 }
